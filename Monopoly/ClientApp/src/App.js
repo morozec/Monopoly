22802c936309fc7d77ffcae422c2ea8799c89568
MorozovAP <andre15@inbox.ru>
2019-07-12 11:20:44 +0400

    hooks order fix

--- Monopoly/ClientApp/src/App.js
+++ Monopoly/ClientApp/src/App.js
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 import Board from './components/Board'
 import Info from './components/Info'
-import Player from './components/Player'
 import './App.css'
 import { useAuth0 } from "./react-auto0-wrapper";
-import { Button } from 'reactstrap'
 import Loading from "./components/Loading";
+
 
 export default function App() {
   const { loading } = useAuth0();
+  const [money, setMoney] = useState(1000)
+  const [properties, setProperties] = useState([])
 
   if (loading) {
     return <Loading />;
   }
-
-  const [money, setMoney] = useState(1000)
-  const [properties, setProperties] = useState([])
+  
 
   const buyProperty = (property) => {
     setMoney(money - property.cost)
